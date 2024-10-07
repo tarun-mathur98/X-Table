87da958ee200311b33f62593ada60f8296630af3
Tarun Mathur <tarun.mathur01@infosys.com>
2024-10-07 16:29:56 +0530

    X-table

---
+++ src/components/XTable.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+const XTable = () => {
+  const initialData = [
+    { date: "2022-09-01", views: 100, article: "Article 1" },
+    { date: "2023-09-01", views: 100, article: "Article 1" },
+    { date: "2023-09-02", views: 150, article: "Article 2" },
+    { date: "2023-09-02", views: 120, article: "Article 3" },
+    { date: "2020-09-03", views: 200, article: "Article 4" },
+  ];
+
+  const [data, setData] = useState(initialData);
+
+  const sortByDate = () => {
+    setData(
+      [...data].sort(
+        (a, b) => new Date(b.date) - new Date(a.data) || b.views - a.views
+      )
+    );
+  };
+
+  const sortByViews = () => {
+    setData(
+      [...data].sort(
+        (a, b) => b.views - a.views || new Date(b.date) - new Date(a.date)
+      )
+    );
+  };
+
+  return (
+    <div>
+      <h1>Date and Views Table</h1>
+      <button onClick={sortByDate}>Sort by Date</button>
+      <button onClick={sortByViews}>Sort by Views</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Views</th>
+            <th>Article</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <tr key={index}>
+              <td>{item.date}</td>
+              <td>{item.views}</td>
+              <td>{item.article}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+export default XTable;
